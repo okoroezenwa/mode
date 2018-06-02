ce8aaedf056c5797e21897e9c24402a4c8ce221f
“Ezenwa <okoroezenwa@bitbucket.org>
2018-06-02 18:23:18 +0100

    Removed initial Mode files

--- Melody/Melody-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "UIImage+UIStatusBarStyle.h"
-#import "UIImageEffects.h"
-#import "MPMusicPlayerController+Other.h"
-#import "MPMediaQuery+Other.h"
